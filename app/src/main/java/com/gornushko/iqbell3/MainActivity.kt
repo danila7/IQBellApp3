2d77ea4593edc3b3b09c94e2b5a5f331c08c6b96
Danila <dghak@bk.ru>
2020-01-05 19:58:44 +0300

    Initial commit

---
+++ app/src/main/java/com/gornushko/iqbell3/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.gornushko.iqbell3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar as Toolbar?)
+    }
+}
